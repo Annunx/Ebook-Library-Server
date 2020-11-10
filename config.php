a36f437e25df1d0de8e4f2199dd63278c9be3f20
Annun <fjyqjsfxy@live.com>
2020-11-10 14:52:35 +0800

    修改配置文件

--- config.php
+++ config.php
@@ -1,7 +1,10 @@
 <?php
+
 $config['settings'] = array(
-    'path' => "library", // 图书文件路径，一定要放在当前站点下，否则有可能读取不到
-    'title' => "Ebook Library", // 标题
-    'user' => 'Annun',
-    'isKindle' => true // 是否在Kindle上访问：false true
+    'path' => "library",                                    // 图书文件路径，一定要放在当前站点下，否则有可能读取不到
+    'title' => "Ebook Library",                             // 标题
+    'user' => 'Annun',                                      // 右侧用户名，当前仅占位使用
+    // 'isKindle' => true,                                  // 是否在 Kindle 上访问：false true，默认会识别设备，如果设备没有被识别，请开启该配置
+    'ext' => array("awz", "txt", "pdf",  "prc", "epub")     // 需要配置的文件格式
 );
+
