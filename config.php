b290d837a4e4b924fdd82811e760e552ed6c3a2c
Annun <fjyqjsfxy@live.com>
2020-11-10 10:41:19 +0800

    修改名称

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $config['settings'] = array(
-    'path' => "files",
-    'title' => "Annun Files",
+    'path' => "library", // 图书文件路径，一定要放在当前站点下，否则有可能读取不到
+    'title' => "Ebook Library", // 标题
     'user' => 'Annun',
     'isKindle' => true // 是否在Kindle上访问：false true
 );
