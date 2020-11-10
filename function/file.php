2b2a719e83f763c911272f2f564de30adc17fcc5
Annun <fjyqjsfxy@live.com>
2020-11-10 14:53:22 +0800

    用户可配置格式移到配置文件

--- function/file.php
+++ function/file.php
@@ -1,4 +1,5 @@
 <?php
+include_once "./config.php";
 //Bytes/Kb/MB/GB/TB/EB
 /**
  * 转换字节大小
@@ -45,14 +46,15 @@ function downFile($filename)
  * @param string $kindle
  * @return boolen
  */
-function fileExtType($ext, $kin)
+function fileExtType($ext, $device)
 {
-	$kin ? $kin : false;
+    global  $config;
+	$device ? $device : false;
 	// 默认仅显示 Kindle 格式
 	// Kindle 支持格式，内置浏览器支支持4中格式
 	$extArr = array("awz", "txt", "mobi", "prc");
-	if (!$kin) {
-		$extArr =  array("awz", "txt", "pdf",  "prc", "epub");
+	if (!$device) {
+		$extArr =  $config['settings']['ext'];
 	};
 	if (in_array($ext, $extArr)) {
 		return true;
